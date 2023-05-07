90746cfdec63eaa311f4891e14243773708cfd8b
Dmitry-kl <i6169696@gmail.com>
2023-05-07 13:08:21 +0300

    init ad add styles

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+import {Link} from "react-router-dom";
+
+export const Header = () => {
+    return (
+        <div className={styles.root}>
+            <Container maxWidth="lg">
+                <div className={styles.inner}>
+                    <Link to='/' className={styles.logo}>
+                        <div>Dmitry BLOG</div>
+                    </Link>
+                    <div className={styles.buttons}>
+                        <Link to='/login'>
+                            <Button variant="outlined">Войти</Button>
+                        </Link>
+                        <Link to='/registration'>
+                            <Button variant="contained">Создать аккаунт</Button>
+                        </Link>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
